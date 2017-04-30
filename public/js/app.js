3481d82d451e3b9ee60dfc97e19556bc726454ad
524855 <annguyen@C02S7148FVH8.home>
2017-04-30 14:31:29 -0400

    get latest changes from Todd's

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,57 @@
-var app = angular.module('BreweryApp', []);
 
-app.controller('MainController', ['$http', function($http){
-   this.getBreweries = function(brew){
-      console.log(brew);
-      $http({
-         method:"GET",
-         url:"http://api.brewerydb.com/v2/locations?key=cbf87c44338b3c02f584632bf9a5cf01&postalCode=" + brew //error not Access-Control-Allow-Origin
-      }).then(function(response){
-         console.log(response);
-      }),function(error){
-         console.log(error);
-      };
-   };
-}]);
+
+console.clear();
+var app = angular.module('MyApp', []);
+
+// User Registration controller
+app.controller('UserController', ['$http', function($http){
+  this.showRegisterForm = false;
+  this.showLoginForm = false;
+  this.isAdmin =false;
+  this.addUser = function(){
+    // console.log('add user');
+        $http({
+            method:'POST',
+            url:'/users',
+            data: {
+                username: this.userName,
+                password: this.password,
+                isAdmin: this.isAdmin
+            }
+        }).then(function(response){ //success
+            console.log("this is respnse " , response);
+        }, function(error){ //fail
+            console.log("angualr: err " , error);
+            alert('The user name is already taken')
+        });
+        this.userName = "";
+        this.password = "";
+        this.isAdmin  = false;
+
+    };
+
+// log in section
+
+ // login method
+ this.logIn = function(){
+  //  console.log("Log in as : ", this.userName);
+   $http({
+       method:'POST',
+       url:'/sessions',
+       data: {
+           username: this.userName,
+           password: this.password,
+       }
+   }).then(function(response){ //success
+       console.log(response.data);
+   }, function(error){ //fail
+        console.log("wrong user name or password");
+        alert("wrong user name or password")
+   });
+   this.userName = "";
+   this.password = "";
+ };
+
+
+
+}]); // end of user controller
