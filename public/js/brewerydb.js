dd7414ba68ba65989f47a3211e6cbab16ddd5e32
Mark De May <markdemay@TirNaNog.PK5001Z>
2017-04-30 11:49:24 -0600

    Added brewerydb controller to app.js

--- public/js/brewerydb.js
+++ public/js/brewerydb.js
@@ -1,18 +1,6 @@
-var app = angular.module('BreweryApp', []);
+var app = angular.module('BreweryDBApp', []);
 
-app.controller('MainController', ['$http', function($http){
-   this.getBreweries = function(brew){
-      console.log(brew);
-      $http({
-         method:"GET",
-         url:"http://api.brewerydb.com/v2/locations?key=cbf87c44338b3c02f584632bf9a5cf01&postalCode=" + brew //error not Access-Control-Allow-Origin
-      }).then(function(response){
-         console.log(response);
-      }),function(error){
-         console.log(error);
-      };
-   };
-}]);
+console.log("BreweryDBApp");
 
 app.controller('BreweryDBController', ['$http', function($http) {
   var controller = this;
