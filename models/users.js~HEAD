3be6d96e36821905ad6feb25f88c4402f1c2f954
524855 <annguyen@C02S7148FVH8.home>
2017-05-01 14:38:12 -0400

    merger and fix problem with users.js

---
+++ models/users.js~HEAD
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose'); // include mongoose
+var Schema = mongoose.Schema;// creating mongoose schema
+
+var userSchema = Schema({
+    username: {type: String, unique: true, required: true },
+    password: {type: String, required: true},
+    isAdmin: Boolean,
+    beers: [],
+    breweries:[],
+
+});
+
+var User = mongoose.model('User', userSchema);
+
+module.exports = User;
---
+++ public/js/brewerydb.js
@@ -0,0 +1,38 @@
+angular.module('BreweryApp').controller('BreweryDBController', ['$http', function($http) {
+
+
+  var controller = this;
+  this.breweries = [];
+
+  this.getBreweriesByZip = function(zipCode) {
+    var urlStr = '/breweries/proxy/v2/locations?postalCode=' + zipCode;
+    $http({
+      method: 'GET',
+      url: urlStr,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      }
+    }).then( function(response) {
+      console.log('here is my log');
+      controller.breweries = response.data.data;
+    }, function(response) {
+      console.log("Get by zip code failed", response);
+      this.breweries = [];
+    });
+  };
+
+  this.getBreweryByName = function(name) {
+    var urlStr = 'breweries/proxy/v2/breweries?name=' + name;
+
+    $http({
+      method: 'GET',
+      url: urlStr
+    }).then( function(response) {
+      controller.breweries = response.data;
+    }, function(response) {
+      console.log("getBreweryByName failed:", response);
+   });
+  };
+
+
+}]);
