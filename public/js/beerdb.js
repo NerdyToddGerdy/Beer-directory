1737a6eb84e23dac0f6fb87cbe7f618f264747c2
Mark De May <markdemay@TirNaNog.PK5001Z>
2017-05-01 18:49:58 -0600

    Search for beers by brewery

--- public/js/beerdb.js
+++ public/js/beerdb.js
@@ -4,23 +4,38 @@ angular.module('BreweryApp').controller('BeerDBController', ['$http', function($
   this.beers = [];
   this.selectedBeer = "";
   this.foundNoBeers = false;
+  this.searchForBeer = "";
+  this.searchByBrewery = "";
+
+  this.clearSearchForm = function() {
+    controller.searchForBeer = "";
+    controller.beers = [];
+    this.searchByBrewery = "";
+  }
 
   this.addFoundBeersToList = function(data) {
     for (var i = 0; i < data.length; i++ ) {
       var newBeer = {
         name: data[i].name,
-        description: data[i].style.description,
+        description: data[i].description,
         style: data[i].style.shortName,
         abv: data[i].abv,
         ibu: data[i].ibu
       }
+      controller.beers.push(newBeer);
     }
-    controller.beers.push(newBeer);
   }
 
+  this.findBeer = function() {
+    if (this.searchForBeer !== "") {
+      this.getBeerByName();
+    } else if (this.searchByBrewery !== "") {
+      this.getBeersByBrewery();
+    }
+  }
 
-  this.getBeerByName = function(name) {
-    var urlStr = '/breweries/proxy/v2/beers?name=' + name;
+  this.getBeerByName = function() {
+    var urlStr = '/breweries/proxy/v2/beers?name=' + controller.searchForBeer;
 
     $http({
       method: 'GET',
@@ -28,7 +43,6 @@ angular.module('BreweryApp').controller('BeerDBController', ['$http', function($
     }).then( function(response) {
       controller.beers = [];
 
-      console.log(response);
       if (response.data.hasOwnProperty('data')) {
         controller.foundNoBeers = false;
       }
@@ -42,11 +56,10 @@ angular.module('BreweryApp').controller('BeerDBController', ['$http', function($
       console.log("Get beer by name failed", response);
       controller.beers = [];
     }
-  )};
+    )};
 
   this.getBreweryByBeerID = function(beerID) {
-    var urlStr = "/breweries/proxy/v2/beer/" + beerID + "/breweries"
-
+    var urlStr = "/breweries/proxy/v2/beer/" + beerID + "/breweries";
 
     $http({
       method: 'GET',
@@ -54,11 +67,46 @@ angular.module('BreweryApp').controller('BeerDBController', ['$http', function($
     }).then( function(response) {
       if (response.data.hasOwnProperty('data') === true) {
         controller.beers[0].brewery = response.data.data[0].name;
-      };
-    }), function(response) {
-      console.log("getBreweryByBeerID failed:", err);
-    };
-  };
+      }
+    }, function(response) {
+      console.log("getBreweryByBeerID failed:", resonse);
+    }
+  )};
+
+  this.getBeersByBrewery = function() {
+    controller.beers = [];
+    var urlStr = "/breweries/proxy/v2/breweries?name=" + controller.searchByBrewery;
+
+    $http({
+      method: 'GET',
+      url: urlStr
+    }).then( function(response) {
+      if (response.data.hasOwnProperty('data')) {
+        controller.getBreweryBeers(response.data.data[0].id);
+      }
+    }, function(response) {
+      console.log("getBeersByBrewery failed", response);
+    })
+  }
+
+  this.getBreweryBeers = function(breweryID) {
+    var urlStr = "/breweries/proxy/v2/brewery/" + breweryID + "/beers";
+
+    $http({
+      method: 'GET',
+      url: urlStr
+    }).then(function(response) {
+      if (response.data.hasOwnProperty('data')) {
+        controller.foundNoBeers = false;
+      } else {
+        controller.foundNoBeers = true;
+        return;
+      }
+      controller.addFoundBeersToList(response.data.data);
+    }, function(response) {
+      console.log("getBreweryBeers failed:", response);
+    })
+  }
 
 }]);
 
